7088b90e17fe527e663c397e0062825a028d703f
Aditya-Bhakhar <adityabhakhar512@gmail.com>
2025-07-14 23:51:34 +0530

    serverless-http added

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -17,6 +17,7 @@ import helmet from "helmet";
 import listenForNewInvoices from "./services/listenForNewInvoices.js";
 
 import { ActivityLogService } from "./services/activityLogService.js";
+import serverless from 'serverless-http';
 import pool from "./db/pool.js";
 import { createActivityLogger } from "./middlewares/activityLogMiddleware.js";
 
@@ -91,6 +92,8 @@ app.use("/api", routes);
 app.use(errorHandler);
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Serving on "http://${HOST}:${PORT}"`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Serving on "http://${HOST}:${PORT}"`);
+// });
+
+export const handler = serverless(app);
